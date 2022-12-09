82e143d121358e99c6748754c6e00248a6b44c0f
Marcel <pabstmarcel95@gmail.com>
2022-12-09 23:40:42 +0100

    Make output more readable

--- cardano-accointing-exporter.py
+++ cardano-accointing-exporter.py
@@ -7,26 +7,21 @@ import endpoints.koios as koios
 import config
 
 from shared import data_handler
-from config import BLOCKFROST_BASE_API, SHELLEY_START_EPOCH, SHELLEY_START_DATETIME, KOIOS_BASE_API
+from config import BLOCKFROST_BASE_API, SHELLEY_START_EPOCH, SHELLEY_START_DATETIME, KOIOS_BASE_API, LINE_CLEAR
 from shared.helper import *
 
-# Variables
-wallet_files = glob.glob('wallets/*.wallet')
-wallet_counter = len(wallet_files)
-calculated_wallet_counter = 0
-address_counter = 0
-calculated_address_counter = 0
-stake_key = None
 
 # Start of the script itself
 config.init()
 requests_cache.install_cache(expire_after=None)
 
-for wallet in wallet_files:
+for wallet in config.wallet_files:
 
-    calculated_wallet_counter += 1
-    reward_counter = 0
-    print('Calculating wallet ' + str(calculated_wallet_counter) + ' of ' + str(wallet_counter))
+    config.calculated_wallet_counter += 1
+    config.tx_counter = 0
+    config.reward_counter = 0
+
+    print('Calculating wallet ' + str(config.calculated_wallet_counter) + ' of ' + str(config.wallet_counter))
     print('-- Reading wallet ' + wallet)
     csv_data = []
     stake_keys_calculated = set()
@@ -50,17 +45,18 @@ for wallet in wallet_files:
             page += 1
             for address in addresses_r.json():
                 addresses.append(address['address'])
-                print('-------- Address found ' + address['address'])
+                print('-------- Address found ' + address['address'], end='\r')
 
-    address_counter += len(addresses)
+    config.address_counter += len(addresses)
     # Wallet Transaction History
     for address in addresses:
 
-        calculated_address_counter += 1
+        config.calculated_address_counter += 1
         clear()
         config.elapsed_time = time.time() - config.start_time
-        print('Calculating wallet ' + str(calculated_wallet_counter) + ' of ' + str(wallet_counter) + ' - Elapsed Time: ' + str(round(config.elapsed_time, 2)))
-        print('Calculating address ' + str(calculated_address_counter) + ' of ' + str(address_counter))
+        print()
+        print('Calculating wallet ' + str(config.calculated_wallet_counter) + ' of ' + str(config.wallet_counter) + ' - Elapsed Time: ' + str(round(config.elapsed_time, 2)))
+        print('Calculating address ' + str(config.calculated_address_counter) + ' of ' + str(config.address_counter))
 
         # Address request
         addr_r = blockfrost.request_api(BLOCKFROST_BASE_API + 'addresses/' + address)
@@ -87,7 +83,7 @@ for wallet in wallet_files:
                 reward_history = [item for sublist in reward_history for item in sublist]
 
                 for reward in reward_history[0]['rewards']:
-                    reward_counter += 1
+                    config.reward_counter += 1
                     datetime_delta = (reward['earned_epoch'] - SHELLEY_START_EPOCH) * 5
                     reward_time = SHELLEY_START_DATETIME + timedelta(days=datetime_delta) + timedelta(days=10)
                     amount = int(reward['amount']) / 1000000
@@ -100,7 +96,7 @@ for wallet in wallet_files:
                     elif reward_type == 'treasury' or reward_type == 'reserves':
                         classification = 'bounty'
                     deposit = ['deposit', reward_time.strftime('%m/%d/%Y %H:%M:%S'), amount, 'ADA', '', '', '', '',
-                               classification, '', '', '', reward_counter]
+                               classification, '', '', '', config.reward_counter]
                     data_handler.add_row(deposit, csv_data)
             else:
                 print('---- skipping rewards already calculated for ' + stake_key)
@@ -118,29 +114,32 @@ for wallet in wallet_files:
                 addr_txs_r = blockfrost.request_api(BLOCKFROST_BASE_API + 'addresses/' + address + '/transactions' + '?page=' + str(page))
                 new_results = addr_txs_r.json()
                 addr_txs.append(addr_txs_r.json())
+                config.elapsed_time = time.time() - config.start_time
+                config.tx_counter += len(new_results)
                 page += 1
+                print(LINE_CLEAR + '---- Received ' + str(config.tx_counter) + ' TXs - Elapsed Time: ' + str(round(config.elapsed_time, 2)), end='\r')
 
         addr_txs = [item for sublist in addr_txs for item in sublist]
 
         # Get detailed transaction information
-        print('-- Get detailed transaction information')
+        print('\n-- Get detailed transaction information')
         txs_details = []
         for tx in addr_txs:
-            print('---- for transaction ' + tx['tx_hash'])
+            print(LINE_CLEAR + '---- for transaction ' + tx['tx_hash'], end='\r')
             tx_details_r = blockfrost.request_api(BLOCKFROST_BASE_API + 'txs/' + tx['tx_hash'])
             txs_details.append(tx_details_r.json())
 
         # Get UTXOs for all transactions
-        print('-- Get transaction UTXOs')
+        print('\n-- Get transaction UTXOs')
         txs_utxos = []
 
         for tx in txs_details:
-            print('---- for transaction ' + tx['hash'])
+            print(LINE_CLEAR + '---- for transaction ' + tx['hash'], end='\r')
             tx_utxo_r = blockfrost.request_api(BLOCKFROST_BASE_API + 'txs/' + tx['hash'] + '/utxos')
             txs_utxos.append([tx_utxo_r.json(), tx])
 
         # Filter inputs and outputs
-        print('-- Filter inputs and outputs')
+        print('\n-- Filter inputs and outputs')
         inputs = []
         outputs = []
         reward_withdrawals = []
@@ -206,5 +205,5 @@ data_handler.convert_csv_to_xlsx(glob.glob('wallets/*.csv'))
 end_time = time.time()
 config.elapsed_time = end_time - config.start_time
 print('\nTransaction history created successfully in ' + str(round(config.elapsed_time, 4)) + 's using ' + str(config.cache_counter) +
-      ' cached calls and ' + str(config.api_counter) + ' API calls for ' + str(len(wallet_files)) + ' wallet/s with ' +
-      str(address_counter) + ' address/es.')
+      ' cached calls and ' + str(config.api_counter) + ' API calls for ' + str(len(config.wallet_files)) + ' wallet(s) with ' +
+      str(config.address_counter) + ' address(/)es).')
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
+import glob
 import time
 from datetime import datetime
-
 
 # Configuration variables
 PROJECT_ID = ''
@@ -8,6 +8,7 @@ BLOCKFROST_BASE_API = 'https://cardano-mainnet.blockfrost.io/api/v0/'
 KOIOS_BASE_API = 'https://api.koios.rest/api/v0/'
 SHELLEY_START_EPOCH = 208
 SHELLEY_START_DATETIME = datetime(2020, 7, 29, 21, 44, 51)
+LINE_CLEAR = '\x1b[2K'
 
 
 def init():
@@ -17,9 +18,24 @@ def init():
     global start_time
     global request_time
     global elapsed_time
+    global calculated_wallet_counter
+    global address_counter
+    global calculated_address_counter
+    global tx_counter
+    global reward_counter
+    global wallet_files
+    global current_wallet
+    global wallet_counter
 
+    wallet_files = glob.glob('wallets/*.wallet')
+    wallet_counter = len(wallet_files)
     api_counter = 0
     cache_counter = 0
+    calculated_wallet_counter = 0
+    address_counter = 0
+    calculated_address_counter = 0
+    tx_counter = 0
+    reward_counter = 0
     start_time = time.time()
     request_time = time.time()
     elapsed_time = time.time() - start_time
